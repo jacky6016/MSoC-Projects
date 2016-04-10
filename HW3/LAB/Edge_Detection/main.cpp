54b43a1be504805296627a43aa945093b57c357d
林展翔 <e24019067@mail.ncku.edu.tw>
2016-04-10 08:47:02 +0800

    Add LAB3 files(practice unfinished)

---
+++ HW3/LAB/Edge_Detection/main.cpp
@@ -0,0 +1,28 @@
+#include <systemc.h>
+
+#include "simple_bus.h"
+#include "simple_bus_arbiter.h"
+#include "edge_detect.h"
+#include "frame_buffer.h"
+#include "display_interface.h"
+int sc_main(int, char **)
+{
+    /*-	Signal Declaration	-*/
+    sc_clock C1;
+
+    /*-	Instantiation	-*/
+    simple_bus          iBus("iBus");
+    simple_bus_arbiter	iArbiter("iArbiter");
+    edge_detect	        iEdge_detect("iEdge_detect",true,512,512);
+    frame_buffer        iFrame_buffer("iFrame_buffer",0x00000,0xfffff);
+    display_interface   iDisplay_interface("iDisplay_interface",0x100000,0x100003);
+
+
+    /*-	Net Connection	-*/
+
+
+
+    sc_start(10000, SC_NS);
+
+    return 0;
+}
