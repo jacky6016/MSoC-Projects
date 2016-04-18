fc2b2f9723fb14ca55ef31b7d8a576c8bc140cec
林展翔 <e24019067@mail.ncku.edu.tw>
2016-04-18 21:50:00 +0800

    Add problem 1 & 2 files(not completed).

---
+++ HW3/problem_1/master_wrapper.h
@@ -0,0 +1,7 @@
+#ifndef __MASTER_WRAPPER_H__
+#define __MASTER_WRAPPER_H__
+#include <systemc.h>
+
+
+
+#endif
---
+++ HW3/problem_1/slave_wrapper.h
@@ -0,0 +1,7 @@
+#ifndef __SLAVE_WRAPPER_H__
+#define __SLAVE_WRAPPER_H__
+#include <systemc.h>
+
+
+
+#endif
